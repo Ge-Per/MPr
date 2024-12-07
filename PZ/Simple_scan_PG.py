dd27b4563f61361d7b66a1b2e0901518175ccab9
Ge-Per <97132622+Ge-Per@users.noreply.github.com>
2024-12-07 15:41:07 +0200

    Fix timeout

--- PZ/Simple_scan_PG.py
+++ PZ/Simple_scan_PG.py
@@ -5,7 +5,7 @@ def scan_ports(ip, start_port, end_port):
     print(f"Scanning ports {start_port} to {end_port} on {ip}...")
     for port in tqdm(range(start_port, end_port + 1), desc="Scanning"):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.settimeout(1)
+            s.settimeout(0.5)
             result = s.connect_ex((ip, port))  # 0 if open
             if result == 0:
                 print(" < is open")
